899f8bf9d9c5bd51d692c02a5107aba4bc78efde
SavageLM <nagol.egavas@gmail.com>
2024-01-23 20:14:01 -0600

    Added argv sorter to get file name and options, filtered print based on flags

--- ls/_ls.c
+++ ls/_ls.c
@@ -9,8 +9,8 @@
 
 int main(int argc, const char *argv[])
 {
-	int flag_1 = 0, flag_a = 0, flag_l = 0; flag_A = 0, i = 0;
-	char file_name;
+	int flag_1 = 0, flag_a = 0, flag_l = 0, flag_A = 0, i = 1, j = 1;
+	const char *file_name;
 
 	if (argc == 1)
 	{
@@ -19,28 +19,32 @@ int main(int argc, const char *argv[])
 
 	else if (argc > 1)
 	{
-		if (argv[1][0] == '-')
+		for (; j <= argc; j++)
 		{
-			for (i; argv[1][i] != NULL, i++)
+			if (argv[j][0] == '-')
 			{
-				if (argv[1][i] == 1)
-					flag_1 = 1;
-				else if (argv[1][i] == 'l')
-					flag_l = 1;
-				else if (argv[1][i] == 'a')
-					flag_a = 1;
-				else if (argv[1][i] == 'A')
-					flag_A = 1;
-				else
+				for (; argv[j][i] != '\0'; i++)
 				{
-					/** Error Code goes here*/
+					if (argv[j][i] == 1)
+						flag_1 = 1;
+					else if (argv[j][i] == 'l')
+						flag_l = 1;
+					else if (argv[j][i] == 'a')
+						flag_a = 1;
+					else if (argv[j][i] == 'A')
+						flag_A = 1;
+					else if (argv[j][i] == '1')
+						flag_1 = 1;
+					else
+					{
+						perror("./hls: Option not supported");
+					}
 				}
 			}
-			file_name = argv[argc - 1];
+			else
+				file_name = argv[j];
 		}
-		else
-			file_name = argv[1];
-		new_ls(file_name, flag_1, flag_A, flag_a, flag_l);
+		new_ls(file_name, flag_a, flag_l, flag_1, flag_A);
 	}
 	return (0);
 }
@@ -56,28 +60,41 @@ int main(int argc, const char *argv[])
 
 void new_ls(const char *dir, int flag_a, int flag_l, int flag_1, int flag_A)
 {
-	struct dirent *res = readdir(dh);
+	struct dirent *res;
 	DIR *dh = opendir(dir);
 
 	if (!dh)
 	{
 		if (errno = ENOENT)
-			perror("Directory Doesn't Exist");
+			perror("./hls: ");
 		else
 			perror("Unable to read directory");
 		exit(EXIT_FAILURE);
 	}
 
-	while (res)
+	while ((res = readdir(dh)))
 	{
 		if (flag_l)
 		{
 			continue;
 			/** Added in code for -l later*/
 		}
-		else if (!flag_a && res->d_name[0] == '.')
-			continue;
-		printf("%s ", res->d_name);
+		else if (flag_A)
+		{
+			if (res->d_name[0] == '.' && res->d_name[1] == '\0')
+				continue;
+			else if (res->d_name[1] == '.' && res->d_name[2] == '\0')
+				continue;
+			printf("%s ", res->d_name);
+		}
+		else if (!flag_a)
+		{
+			if (res->d_name[0] == '.')
+				continue;
+			printf("%s ", res->d_name);
+		}
+		else
+			printf("%s ", res->d_name);
 		if (flag_1)
 			printf("\n");
 	}
