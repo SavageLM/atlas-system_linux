453e7df5b87aa0790e11dae590da63cc867ec337
SavageLM <nagol.egavas@gmail.com>
2024-03-13 10:34:09 -0500

    Changed some registries in 4-strstr

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -12,16 +12,16 @@ asm_strstr:
     xor rcx, rcx ; sets counter to 0
 asm_strstr_search:
     push rsi
-    cmp rdi, rsi ; checks if bytes are same
+    cmp dil, sil ; checks if bytes are same
     jz substr_start ; if match, jump to check substring
-    cmp byte [rdi], 0 ; check if rdi is NULL
+    cmp dil, 0x00 ; check if rdi is NULL
     jz asm_strstr_null ; jumps if rdi is NULL
     inc rdi
     jmp asm_strstr_search; Loop
 
 substr_start:
     xor rax, rax ; make sure rax is clear
-    cmp byte [rdi], 0 ; check if rdi is NULL
+    cmp dil, 0x00 ; check if rdi is NULL
     jz asm_strstr_null ; jumps if rdi is NULL
     mov rax, rdi ; sets start position of substring
     jmp substr_loop ; jumps to lopping substr
@@ -29,11 +29,11 @@ substr_start:
 substr_loop:
     inc rdi
     inc rsi
-    cmp rdi, rsi ; check if still match
+    cmp dil, sil ; check if still match
     jz substr_loop ; loops back
-    cmp byte [rsi], 0 ; check if end of substring
+    cmp sil, 0x00 ; check if end of substring
     jz asm_strstr_end
-    cmp byte [rdi], 0 ; check if rdi is NULL
+    cmp dil, 0x00 ; check if rdi is NULL
     jz asm_strstr_null ; jumps if rdi is NULL
     pop rsi ; resotres rsi on a non-match
     jmp asm_strstr_search ; starts search over
