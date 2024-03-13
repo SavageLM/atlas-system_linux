18f4f734d379671415ced0a79a19941a2025c5e8
SavageLM <nagol.egavas@gmail.com>
2024-03-13 11:01:42 -0500

    rewroter strchr

--- libasm/3-strchr.asm
+++ libasm/3-strchr.asm
@@ -12,12 +12,12 @@ asm_strchr:
     xor rax, rax ; ensures rax is 0 at start
 
 asm_strchr_next:
-    cmp dil, sil ; checks if bytes are same
+    movzx ecx byte [rdi]
+    cmp cl, sil ; checks if bytes are same
     jz asm_strchr_match
-    cmp dil, 0x00
-    jz asm_strchr_end
     inc rdi
-    jmp asm_strchr_next; Loop
+    test cl, cl
+    jnz asm_strchr_next; Loop
 
 asm_strchr_match:
     mov rax, rdi
