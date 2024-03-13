c999632f13ed87b64c6652844281bd9825899e2d
SavageLM <nagol.egavas@gmail.com>
2024-03-13 15:13:55 -0500

    corrected 8-strcasecmp

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -10,43 +10,46 @@ asm_strcasecmp:
     push rbp
     mov rbp, rsp  ; Setup stack
     xor rax, rax ; ensures rax is 0 at start
-    push r13
+    push rbx
+    push rcx
 
 asm_strcasecmp_next:
-    mov rax, rdi
-	mov rdx, rsi
-	movzx eax, BYTE [rax]
-	movzx edx, BYTE [rdx]
-    cmp al, 0x00
-    jz asm_strcasecmp_null
-    cmp al, 65 ; compares char to 'A'
+	movzx ebx, BYTE [rdi]
+	movzx ecx, BYTE [rsi]
+    cmp bl, 0x00
+    jnz asm_strcasecmp_bl_cnvt
+    cmp cl, 0x00
+    jnz asm_strcasecmp_cl_cnvt
+    jmp asm_strcasecmp_ne
+
+asm_strcasecmp_bl_cnvt:
+    cmp bl, 65 ; compares char to 'A'
+    jl asm_strcasecmp_cl_cnvt ; if less, is not a letter
+    cmp bl, 90 ;  compares char to 'Z'
+    jg asm_strcasecmp_cl_cnvt ; if greater is already lowercase, or not a letter
+    add bx, 32 ; changes letter to lowercase
+    jmp asm_strcasecmp_cl_cnvt
+
+asm_strcasecmp_cl_cnvt:
+    cmp cl, 65 ; compares char to 'A'
     jl asm_strcasecmp_cmp ; if less, is not a letter
-    cmp al, 90 ;  compares char to 'Z'
+    cmp cl, 90 ;  compares char to 'Z'
     jg asm_strcasecmp_cmp ; if greater is already lowercase, or not a letter
-    add ax, 32 ; changes letter to lowercase
+    add cx, 32 ; changes letter to lowercase
     jmp asm_strcasecmp_cmp
 
 asm_strcasecmp_cmp:
-    cmp al, dl ; checks if bytes are same
-    ja asm_strcasecmp_pos ; jumps to end for positive diff
-    jb asm_strcasecmp_neg ; jumps to end for negative diff
+    cmp bl, cl ; checks if bytes are same
+    jnz asm_strcasecmp_ne ; jumps to end for positive diff
     inc rdi
     inc rsi ; incrementing strings
     jmp asm_strcasecmp_next; Loop
 
-asm_strcasecmp_null:
-    cmp dl, 0x00
-    jl asm_strcasecmp_neg
-    xor rax, rax
-    jmp asm_strcasecmp_end
-asm_strcasecmp_neg:
-    sub rax, 1
-    jmp asm_strcasecmp_end
+asm_strcasecmp_ne:
+    sub rbx, rcx
+    mov rax, rbx
 
-asm_strcasecmp_pos:
-    add rax, 1
-    jmp asm_strcasecmp_end
-
-asm_strcasecmp_end:
+    pop rcx
+    pop rbx
     pop rbp
     ret ; exit
