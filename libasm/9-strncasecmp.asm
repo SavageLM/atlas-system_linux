b76850686000749545fc6a66d27685efd96f568d
SavageLM <nagol.egavas@gmail.com>
2024-03-13 15:11:31 -0500

    re-wrote some insctructions in 9-strncasecmp

--- libasm/9-strncasecmp.asm
+++ libasm/9-strncasecmp.asm
@@ -10,55 +10,49 @@ asm_strncasecmp:
     push rbp
     mov rbp, rsp  ; Setup stack
     xor rax, rax ; ensures rax is 0 at start
+    push rbx
+    push rcx
 
 asm_strncasecmp_next:
     cmp rdx, 0
-    jz asm_strncasecmp_end
-    mov rax, rdi
-	mov rcx, rsi
-	movzx eax, BYTE [rax]
-	movzx edx, BYTE [rdx]
-    cmp al, 0x00
-    jz asm_strncasecmp_null
-    cmp al, 65 ; compares char to 'A'
-    jl asm_strncasecmp_cnvt ; if less, is not a letter
-    cmp al, 90 ;  compares char to 'Z'
-    jg asm_strncasecmp_cnvt ; if greater is already lowercase, or not a letter
-    add al, 32 ; changes letter to lowercase
-    jmp asm_strncasecmp_cnvt
-
-asm_strncasecmp_cnvt:
+    jz asm_strncasecmp_ne
+	movzx ebx, BYTE [rdi]
+	movzx ecx, BYTE [rsi]
+    cmp bl, 0x00
+    jnz asm_strncasecmp_bl_cnvt
     cmp cl, 0x00
-    jz asm_strncasecmp_null
+    jnz asm_strncasecmp_cl_cnvt
+    jmp asm_strncasecmp_ne
+
+asm_strncasecmp_bl_cnvt:
+    cmp bl, 65 ; compares char to 'A'
+    jl asm_strncasecmp_cl_cnvt ; if less, is not a letter
+    cmp bl, 90 ;  compares char to 'Z'
+    jg asm_strncasecmp_cl_cnvt ; if greater is already lowercase, or not a letter
+    add bx, 32 ; changes letter to lowercase
+    jmp asm_strncasecmp_cl_cnvt
+
+asm_strncasecmp_cl_cnvt:
     cmp cl, 65 ; compares char to 'A'
     jl asm_strncasecmp_cmp ; if less, is not a letter
     cmp cl, 90 ;  compares char to 'Z'
     jg asm_strncasecmp_cmp ; if greater is already lowercase, or not a letter
-    add cl, 32 ; changes letter to lowercase
+    add cx, 32 ; changes letter to lowercase
     jmp asm_strncasecmp_cmp
 
 asm_strncasecmp_cmp:
-    cmp al, dl ; checks if bytes are same
-    ja asm_strncasecmp_pos ; jumps to end for positive diff
-    jb asm_strncasecmp_neg ; jumps to end for negative diff
+    cmp bl, cl ; checks if bytes are same
+    jnz asm_strncasecmp_ne ; jumps to end for positive diff
     inc rdi
     inc rsi ; incrementing strings
     dec rdx
     jmp asm_strncasecmp_next; Loop
 
-asm_strncasecmp_null:
-    cmp dl, 0x00
-    jl asm_strncasecmp_neg
-    xor rax, rax
-    jmp asm_strncasecmp_end
-asm_strncasecmp_neg:
-    sub rax, 1
-    jmp asm_strncasecmp_end
+asm_strncasecmp_ne:
+    sub rbx, rcx
+    mov rax, rbx
 
-asm_strncasecmp_pos:
-    add rax, 1
-    jmp asm_strncasecmp_end
-
-asm_strncasecmp_end:
+    pop rcx
+    pop rbx
     pop rbp
     ret ; exit
--- libasm/main.c
+++ libasm/main.c
@@ -5,16 +5,32 @@
 
 #include "libasm.h"
 
+#define S1  "Holberton School"
+#define S2  "HOLBERTON SCHOOL"
+#define S3  "Holberton SchooL"
+#define S4  "holberton socool"
+
 /**
  * main - Program entry point
  *
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
 int main(void)
-{
-    assert(asm_puts("H") == 1);
-    assert(asm_puts("olberton") == 8);
-    assert(asm_puts(" School\n") == 8);
+{  
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S3, 1), asm_strncasecmp(S1, S3, 1));
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S2, 11), asm_strncasecmp(S1, S2, 11));
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S3, 11), asm_strncasecmp(S1, S3, 11));
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S2, 16), asm_strncasecmp(S1, S2, 16));
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S3, 16), asm_strncasecmp(S1, S3, 16));
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S4, 11), asm_strncasecmp(S1, S4, 11));
+    printf("Real: %d, mine: %d/n", strncasecmp(S1, S4, 16), asm_strncasecmp(S1, S4, 16));
+    assert(strncasecmp(S1, S3, 1) == asm_strncasecmp(S1, S3, 1));
+    assert(strncasecmp(S1, S2, 11) == asm_strncasecmp(S1, S2, 11));
+    assert(strncasecmp(S1, S3, 11) == asm_strncasecmp(S1, S3, 11));
+    assert(strncasecmp(S1, S2, 16) == asm_strncasecmp(S1, S2, 16));
+    assert(strncasecmp(S1, S3, 16) == asm_strncasecmp(S1, S3, 16));
+    assert(strncasecmp(S1, S4, 11) == asm_strncasecmp(S1, S4, 11));
+    assert(strncasecmp(S1, S4, 16) == asm_strncasecmp(S1, S4, 16));
 
     printf("All good!\n");
     return (EXIT_SUCCESS);
