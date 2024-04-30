3fca223829cebafc96a0d40286456813be573227
SavageLM <nagol.egavas@gmail.com>
2024-04-30 16:01:02 -0500

    Added 3-strace.c and code

---
+++ strace/2-strace.c
@@ -0,0 +1,45 @@
+#include "syscalls.h"
+
+/**
+ * main - entrance to program
+ * @argc: Number of arguments
+ * @argv: array of arguments
+ * @envp: Environ variable
+ * Return: returns -1 on fail, else 0
+*/
+
+int main(int argc, const char *argv[], char *const envp[])
+{
+	pid_t child;
+	int status, print_check = 0;
+	struct user_regs_struct regs;
+	char const * sysname;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Unsupported number of Arguments\n");
+		return (-1);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		ptrace(PTRACE_TRACEME, child, NULL, NULL);
+		execve(argv[1], (char * const *)(argv + 1), (char * const *)envp);
+	}
+	else
+	{
+		while (1)
+		{
+			ptrace(PT_SYSCALL, child, NULL, NULL);
+			wait(&status);
+			if (WIFEXITED(status))
+				break;
+			ptrace(PTRACE_GETREGS, child, NULL, &regs);
+			sysname = syscalls_64_g[regs.orig_rax].name;
+			if (print_check == 0 || print_check % 2 != 0)
+				fprintf(stderr, "%s = %#lx\n", sysname, (size_t)regs.rax);
+			print_check++;
+		}
+	}
+	return (0);
+}
