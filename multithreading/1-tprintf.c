08c5d7a5ab45b8faf5aed25f6bf985688ca0c598
SavageLM <nagol.egavas@gmail.com>
2024-05-02 11:04:58 -0500

    Removed pthread_exit for a plain return

--- multithreading/1-tprintf.c
+++ multithreading/1-tprintf.c
@@ -3,7 +3,7 @@
 /**
  * tprintf - Function to print thread ID and a str
  * @format: String to print
- * Return: NULL pointer
+ * Return: Always 0
 */
 
 int tprintf(char const *format, ...)
@@ -11,5 +11,5 @@ int tprintf(char const *format, ...)
 	pthread_t tid = pthread_self();
 
 	printf("[%ld] %s", tid, (char *)format);
-	pthread_exit(NULL);
+	return (0);
 }
