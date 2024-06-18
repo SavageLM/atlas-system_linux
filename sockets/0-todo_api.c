19582cbf9ef34e5f298ea1a5062dce6cf7733715
SavageLM <nagol.egavas@gmail.com>
2024-06-18 12:11:51 -0500

    quick test

---
+++ sockets/0-todo_api.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+/**
+ * main - entry to the function
+ * Return: 0 on success
+*/
+int main(void)
+{
+	int socket_fd, new_con, wait;
+	char buffer[1024];
+	struct sockaddr_in address;
+	socklen_t addrlen = sizeof(address);
+
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd == -1)
+		perror("socket failed"), exit(EXIT_FAILURE);
+	address.sin_family = AF_INET, address.sin_port = htons(8080);
+	address.sin_addr.s_addr = INADDR_ANY;
+	if (bind(socket_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+		perror("bind failed"), exit(EXIT_FAILURE);
+	printf("Server listening on port 12345\n");
+	if (listen(socket_fd, 5) < 0)
+		perror("listen failed"), exit(EXIT_FAILURE);
+	new_con = accept(socket_fd, (struct sockaddr *)&address, &addrlen);
+	if (new_con < 0)
+		perror("accept failed"), exit(EXIT_FAILURE);
+	printf("Client connected: %s\n", inet_ntoa(address.sin_addr));
+	while (wait)
+		wait = recv(new_con, buffer, 1024, 0);
+	printf("Message received: \"%s\"\n", buffer);
+	close(new_con);
+	return (0);
+}
--- sockets/README.md
+++ sockets/README.md
@@ -22,7 +22,7 @@
 |1-server.c| Function for opening a socket and accepting a connection|
 |2-client.c| Function to connect to a listening server|
 |3-server.c| Function for opening a socket and listening for a message|
-|
+|0-todo_api.c| Function(s) for parsing and printing HTTP requests|
 
 #### Authors
 Logan Savage
