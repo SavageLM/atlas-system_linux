cd2a0c22bbc1f3a1faeb08ca53f53a9b1ef95148
SavageLM <nagol.egavas@gmail.com>
2024-06-19 14:43:01 -0500

    Fixed 2-client

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -6,6 +6,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
+#ifndef __USE_XOPEN2K
+#define __USE_XOPEN2K
+#endif
 #include <netdb.h>
 
 /**
@@ -17,23 +21,31 @@
 int main(int argc, const char **argv)
 {
 	int client_fd;
-	struct sockaddr_in server_add;
+	/* struct sockaddr_in server_add; */
+	struct hostent *host;
+	struct addrinfo hints = {0}, *addrs, *tmp = NULL;
 
 	if (argc < 3)
 		printf("Usage: %s <host> <port>\n", argv[0]), exit(EXIT_FAILURE);
 
-	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	host = gethostbyname(argv[1]);
+	if (host == NULL)
+		perror("host not found"), exit(EXIT_FAILURE);
+	hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+	if (getaddrinfo(argv[1], argv[2], &hints, &addrs) != 0)
+		perror("Failed to get address info"), exit(EXIT_FAILURE);
+	for (tmp = addrs; tmp != NULL; tmp = tmp->ai_next)
+	{
+		client_fd = socket(addrs->ai_family, addrs->ai_socktype, addrs->ai_protocol);
 	if (client_fd == -1)
 		perror("socket failed"), exit(EXIT_FAILURE);
-
-	server_add.sin_family = AF_INET;
-	server_add.sin_port = htons(12345);
-
-	if (connect(client_fd, (struct sockaddr *) &server_add,
-				sizeof(server_add)) < 0)
-		printf("Failed to Connect\n");
+	if (connect(client_fd, tmp->ai_addr, tmp->ai_addrlen) < 0)
+		perror("Failed to Connect\n"), exit(EXIT_FAILURE);
 	printf("Big Brother is in your walls now...\n");
 	close(client_fd);
-
+	}
+	freeaddrinfo(addrs);
 	return (0);
 }
